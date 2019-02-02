2c1c3c51b37b711470c6875d09216434c9da6620
Dana Simmons <dsanalog@gmail.com>
2019-02-02 17:57:15 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,9 @@
 class Person
   def initialize ( attributes )
-  attributes.each { |key, value| self.send("#{key}", value) }
+  attributes.each do |key, value|
+    self.attr_accessor(key)
+    self.send("#{key}=", value) 
+  end
   end
   #your code here
 end
