74cc87c10155e1de1d054bc757d69cca78efb252
Dana Simmons <dsanalog@gmail.com>
2019-02-02 17:59:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person
   def initialize ( attributes )
   attributes.each do |key, value|
-    Person.attr_reader(key)
+    self.class.attr_reader(key)
     self.send("#{key}=", value) 
   end
   end
